e0815edb0e68f28a387625cd3481a87416112275
linfar <andrey.titov@jetbrains.com>
2018-07-02 14:53:54 +0300

    asdf

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,7 +8,26 @@ namespace dotnetProject
         [Fact]
         public void Test1()
         {
-
+		int a = 10;
+		int b = a + 2;
+		Console.Write(b + a);
         }
+
+        public void Test2()
+        {
+		int a = 10;
+		int b = a + 2;
+		Console.Write(b + a);
+        }
+
+        public void Test3()
+        {
+        }
+
+	[Fact]
+	public void TestFail() 
+	{
+		throw new Exception();
+	}
     }
 }
